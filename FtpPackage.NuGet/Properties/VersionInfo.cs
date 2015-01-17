abfee8b991392e7a31aba6d0f74adc3cd09bffca
fitg <gustaw.fit@gmail.com>
2015-01-17 15:22:37 +0100

    Added NuGet project

---
+++ FtpPackage.NuGet/Properties/VersionInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyCompany("GusLab")]
+[assembly: AssemblyTrademark("https://github.com/GusLab/Ftp")]
+
+[assembly: AssemblyFileVersion("1.0.*")]
+
+#if DEBUG
+[assembly: AssemblyInformationalVersion("1.0-alpha.*")]
+#else
+    [assembly: AssemblyInformationalVersion("1.0.*")]
+#endif
