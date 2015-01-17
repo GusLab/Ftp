ab7ee62b6147cfb22cc6500625fcda69bbd26132
fitg <gustaw.fit@gmail.com>
2015-01-17 15:56:58 +0100

    fixing nunit

--- FtpPackage.NuGet/Properties/VersionInfo.cs
+++ FtpPackage.NuGet/Properties/VersionInfo.cs
@@ -1,14 +1,14 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 [assembly: AssemblyCompany("GusLab")]
 [assembly: AssemblyTrademark("https://github.com/GusLab/Ftp")]
 
-[assembly: AssemblyFileVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.0-alpha.*")]
+[assembly: AssemblyInformationalVersion("1.0-alpha.0.0")]
 #else
-    [assembly: AssemblyInformationalVersion("1.0.*")]
+    [assembly: AssemblyInformationalVersion("1.0.0.0")]
 #endif
