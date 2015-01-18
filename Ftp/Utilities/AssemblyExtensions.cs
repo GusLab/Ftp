8e1f86ddd8945df1ba10d4b0bee76b55a1b71a80
fitg <gustaw.fit@gmail.com>
2015-01-18 19:23:36 +0100

    adding assembly extensions

---
+++ Ftp/Utilities/AssemblyExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using CommandLine;
+
+namespace Ftp.Utilities
+{
+    public static class AssemblyExtensions
+    {
+        public static String GetExecutingAssemblyInformationalVersion(this Assembly executingAssembly)
+        {
+            return ((AssemblyInformationalVersionAttribute)Attribute.GetCustomAttribute(
+            executingAssembly, typeof(AssemblyInformationalVersionAttribute), false))
+           .InformationalVersion; 
+        }
+
+        public static String GetExecutingAssemblyProductName(this Assembly executingAssembly)
+        {
+            var versionInfo = FileVersionInfo.GetVersionInfo(executingAssembly.Location);
+            return versionInfo.ProductName;
+        }
+
+        public static String GetExecutingAssemblyCopyright(this Assembly executingAssembly)
+        {
+            var versionInfo = FileVersionInfo.GetVersionInfo(executingAssembly.Location);
+            return versionInfo.LegalCopyright;
+        }
+
+        public static String GetExecutingAssemblyDescription(this Assembly executingAssembly)
+        {
+            var versionInfo = FileVersionInfo.GetVersionInfo(executingAssembly.Location);
+            return versionInfo.Comments;
+        }
+
+        public static String GetExecutingAssemblyLicence(this Assembly executingAssembly)
+        {
+            return ((AssemblyLicenseAttribute)Attribute.GetCustomAttribute(
+            executingAssembly, typeof(AssemblyLicenseAttribute), false))
+           .Value;
+        }
+
+        public static String GetExecutingAssemblyUsage(this Assembly executingAssembly)
+        {
+            return ((AssemblyUsageAttribute)Attribute.GetCustomAttribute(
+            executingAssembly, typeof(AssemblyUsageAttribute), false))
+           .Value;
+        }
+
+        
+    }
+}
