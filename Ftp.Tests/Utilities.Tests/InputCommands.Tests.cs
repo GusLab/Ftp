7d1c82ef4400b5abcb9e8081905dc61696a3ea5d
fitg <gustaw.fit@gmail.com>
2015-01-18 19:13:55 +0100

    Fixes #1

--- Ftp.Tests/Utilities.Tests/InputCommands.Tests.cs
+++ Ftp.Tests/Utilities.Tests/InputCommands.Tests.cs
@@ -66,7 +66,7 @@ namespace Ftp.UnitTests.Utilities.Tests
                 "-PrP",
                 "password",
                 "-#"
-            };
+            }; 
 
             Assert.IsTrue(false);
             
